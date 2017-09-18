64dcc4883fed32e09e24965219757fdad2d469a6
Alexandru Gaman <alexandru.gaman17@gmail.com>
2017-09-18 16:42:13 +0300

    Added C# project
    Signed-off-by: Alexandru Gaman <alexandru.gaman17@gmail.com>

---
+++ Index.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class Index
+    {
+        public static void Main(String[] args)
+        {
+            int size = 10;
+            
+            int[] arr = Utils.RandArray(size).ToArray(); // Generate the test array
+            int[] calibration = Utils.CalibrationArray(arr).ToArray(); // Generate the "calibration" array
+            
+            Console.WriteLine("Array");
+            foreach (var val in arr) {
+                Console.WriteLine(val);
+            }
+
+            Console.WriteLine("Calibration");
+            foreach (var v in calibration)
+            {
+                Console.WriteLine(v);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ common/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class Utils
+    {
+        public const int ARR_LEN_VALUE = 1000;
+        public const int ARR_MIN_VALUE = 0;
+        public const int ARR_MAX_VALUE = 100000;
+
+        private static readonly Random _random = new Random();
+
+        public static IEnumerable<int> RandArray(int size = ARR_LEN_VALUE, int min = ARR_MIN_VALUE, int max = ARR_MAX_VALUE)
+            => Enumerable.Range(0, size).Select(val => _random.Next(min, max));
+
+        public static IEnumerable<int> CalibrationArray(IEnumerable<int> arr)
+        {
+            var calibration = new int[arr.Count()];
+            Array.Copy(arr.ToArray(), calibration, arr.Count());
+            Array.Sort(calibration);
+
+            return calibration;
+        }
+    }
+}
